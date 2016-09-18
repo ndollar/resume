3ecb06aee4117a922633e122b071c2daae46c8ea
Nick Dollarhide <nixmail427@gmail.com>
2016-09-18 13:01:36 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+/**
+ * DO NOT ADD LOGIC
+ * WE"RE JUST USING NODE TO SERVE STATIC FILES
+ */
+var express = require('express');
+var app = express();
+
+app.use(express.static('./'));
+
+app.listen(3000, function () {
+  console.log('Listening on port 3000');
+});
