fb76d6c08e29b2753f08c23f384856543d0f558a
griffincoding <nixmail427@gmail.com>
2016-09-25 13:43:58 -0500

    Re-responsivized after printing

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('./'));
+app.use('/resume', express.static('./'));
 
 app.listen(3000, function () {
   console.log('Listening on port 3000');
